19e176904d705805f18c033cefd177e730ae3bfc
Rajesh Sharma <er.rajesh22@gmail.com>
2018-10-22 23:59:14 +0530

    initial commit

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+
+@NgModule({
+  declarations: [
+    
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  exports:[
+
+  ]
+})
+export class SharedModule { }
